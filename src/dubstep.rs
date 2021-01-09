8eca463d7cee571f4e1ad11b4728956fbb27e52e
BlueGhostGH <engisoftleaderoff@gmail.com>
2021-01-10 00:27:45 +0200

    Solved "Dubstep"

---
+++ src/dubstep.rs
@@ -0,0 +1,11 @@
+pub fn song_decoder(song: &str) -> String {
+    let original = song.replace("WUB", " ");
+    let original = original.trim();
+
+    let original_words = original.split(" ");
+    let original_non_empty_words = original_words.filter(|word| word != &"");
+    let original_words = original_non_empty_words.collect::<Vec<_>>();
+
+    let original = original_words.join(" ");
+    original
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 pub mod split_strings;
+pub mod dubstep;
 
 fn main() {
     assert_eq!(split_strings::solution("abcdef"), ["ab", "cd", "ef"]);
     assert_eq!(split_strings::solution("abcdefg"), ["ab", "cd", "ef", "g_"]);
+    assert_eq!(dubstep::song_decoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB"), "WE ARE THE CHAMPIONS MY FRIEND")
 }
