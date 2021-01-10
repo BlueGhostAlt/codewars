51c301abb679e6bd0e819e91bcb3051d54cd41d3
BlueGhostGH <engisoftleaderoff@gmail.com>
2021-01-10 11:58:27 +0200

    Solved "Regex validate PIN code"

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 pub mod split_strings;
 pub mod dubstep;
+pub mod regex_validate_pin_code;
 
 fn main() {
     assert_eq!(split_strings::solution("abcdef"), ["ab", "cd", "ef"]);
     assert_eq!(split_strings::solution("abcdefg"), ["ab", "cd", "ef", "g_"]);
-    assert_eq!(dubstep::song_decoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB"), "WE ARE THE CHAMPIONS MY FRIEND")
+    assert_eq!(dubstep::song_decoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB"), "WE ARE THE CHAMPIONS MY FRIEND");
+    assert!(regex_validate_pin_code::validate_pin("1234"));
+    assert!(!regex_validate_pin_code::validate_pin("12345"));
+    assert!(!regex_validate_pin_code::validate_pin("a2345"))
 }
