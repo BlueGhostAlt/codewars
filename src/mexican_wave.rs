9776d2930f2efa6ec6e3c7e1a0051ab8a013757c
BlueGhostGH <engisoftleaderoff@gmail.com>
2021-01-13 13:05:26 +0200

    Refactored solution for "Mexican Wave"

--- src/mexican_wave.rs
+++ src/mexican_wave.rs
@@ -1,22 +1,13 @@
-fn capitalize_letter(word: &str, index: usize) -> String {
-    word.chars()
-        .enumerate()
-        .map(|(i, c)| {
-            if i == index {
-                c.to_uppercase().collect::<String>()
-            } else {
-                c.to_string()
-            }
-        })
-        .collect::<String>()
-}
-
 pub fn wave(s: &str) -> Vec<String> {
-    let len = s.chars().count();
-    let waves = (0..len)
-        .map(|i| capitalize_letter(s, i))
+    s.char_indices()
+        .map(|(i, c)| {
+            format!(
+                "{}{}{}",
+                &s[..i],
+                c.to_uppercase().collect::<String>(),
+                &s[i + 1..]
+            )
+        })
         .filter(|wave| wave != s)
-        .collect::<Vec<_>>();
-
-    waves
+        .collect::<Vec<_>>()
 }
